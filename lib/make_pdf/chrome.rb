74b8a79057a34bd9ae48fdadc14f0467e0c7361c
Victor Bogado da Silva Lins <victor@bogado.net>
2025-08-22 11:25:56 -0400

    Minor updates.
    * Typo fixed

--- lib/make_pdf/chrome.rb
+++ lib/make_pdf/chrome.rb
@@ -4,8 +4,8 @@ module MakePDF
   class Chrome < CommandBased::Writer
     COMMAND = 'chrome-headless-render-pdf'
 
-    def initialize(source_url, output_dif, **options)
-      super(source_url, output_dif, command: COMMAND, **options)
+    def initialize(source_url, output_dir, **options)
+      super(source_url, output_dir, command: COMMAND, **options)
     end
   end
 end
