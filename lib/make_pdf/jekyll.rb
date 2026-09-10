31a7ce18ab9599f8a13fe3b3fbca08904df52cd3
Victor Bogado da Silva Lins <victor@bogado.net>
2026-09-10 15:38:42 -0400

    Update gem:
    * Store Hash for files

--- lib/make_pdf/jekyll.rb
+++ lib/make_pdf/jekyll.rb
@@ -1,13 +1,108 @@
 require 'jekyll'
 require 'make_pdf'
 require 'path_of'
+require 'digest'
+require 'uri'
 
 module MakePDF
-  LOG_NAME = "make_pdf:"
+  LOG_NAME = 'make_pdf:'.freeze
+
+  # Site
+  #
+  # Responible for bridging the configurartions on Jekyll and setting up
+  # the conversion of PDFs
+  class MakePDFSite
+    include PathManip
+    attr_reader :options, :site, :logger, :file
+
+    def default_options
+      {
+        output_base_path: site.dest,
+        input_location: path_of(site.dest),
+        input_base_url: relative_path_of(site.baseurl[1..]),
+        input_host: URI(site.config['url']).host,
+        input_scheme: 'file'
+      }.freeze
+    end
+
+    def make_options(options, *more_options)
+      return {} if options.nil?
+
+      [options, more_options].flatten
+                             .reduce(:merge)
+                             .transform_keys do |key|
+                               key.to_s.sub('-', '_').to_sym
+                             end
+    end
+
+    def initialize(site, **options)
+      raise 'site is nil' if site.nil?
+
+      @site    = site
+      config   = site.config['make-pdf'] || {}
+      @logger  = MakePDF::Logger.new(logger: ::Jekyll.logger, level: config['log-map-level'] || :info)
+      @options = default_options.merge(make_options(@site.config['make-pdf'], options))
+      @queue   = []
+      @hashes  = site.data[metadata] || {}
+
+      logger.debug("Initialized with #{self.options}.")
+    end
+
+    def queue(processor)
+      logger.verbose("Adding #{processor.name} to queue")
+      @queue.push(processor)
+    end
+
+    def <<(doc)
+      processor = Processor.new(self, doc, **@options)
+      if processor.valid?
+        queue(processor)
+      else
+        logger.verbose("Skip #{doc.name} => #{processor.reason}")
+      end
+    end
+
+    def metadata
+      'make_pdf_hashes'
+    end
+
+    def metadata_dir
+      result = Pathname.new(site.config['data_dir'])
+      Dir.mkdir(result) unless Dir.exist?(result)
+
+      result
+    end
+
+    def metadata_file
+      metadata_dir / Pathname.new("#{metadata}.yaml")
+    end
+
+    def save_hashes
+      File.open(metadata_file, 'w') do |file|
+        file.write(@hashes.to_yaml)
+      end
+    end
+
+    def process
+      @queue.each do |processor|
+        hash = processor.filehash
+        if hash != @hashes[processor.file]
+          processor.process(**@options)
+          @hashes[processor.source.to_s] = hash
+        else
+          processor.skip('Not changed')
+        end
+        processor
+      end
+      save_hashes
+    end
+  end
 
   # MakePDF Jekyll plugin
+  #
+  # Processor is the class that process a single document.
   class Processor
-    attr_reader :reason, :doc, :name
+    attr_reader :reason, :doc, :name, :source, :file
 
     def valid?
       @reason.nil?
@@ -18,39 +113,50 @@ module MakePDF
       condition
     end
 
+    def enable(value)
+      possible = {
+        '' => true,
+        'true' => true,
+        'yes' => true,
+        'false' => false,
+        'no' => false
+      }
+
+      ['make-pdf', possible[value.to_s.downcase]]
+    end
+
     def filter_options(document, **options)
       document.data.filter_map do |key, value|
         key = key.to_s
-        if key == "make-pdf"
-          possible = {
-            "" => true,
-            "true" => true,
-            "yes" => true,
-            "false" => false,
-            "no" => false
-          }
-
-          [ key, possible[value.to_s.downcase] ]
-        else
-          [ key.sub("make-pdf-", "").to_sym, value ] if key.start_with?("make-pdf-")
+        if key == 'make-pdf'
+          enable(value)
+        elsif key.start_with?('make-pdf-')
+          [key.sub('make-pdf-', '').to_sym, value]
         end
       end.to_h.merge(options)
     end
 
-    def initialize(site, current_doc, **options)
-      @site    = site
-      @file    = current_doc.destination(@base_source)
-      @options = filter_options(current_doc, **options)
-      @name    = current_doc.name
+    def skip(reason)
+      logger.verbose("Skipped #{@file} #{reason}")
+    end
 
-      logger.debug("base_paths: input → #{@options[:input_base_url]} output → #{@options[:output_base_path]} host → #{@options[:input_host]}")
+    def initialize(site, current_doc, **options)
+      @site     = site
+      @file     = current_doc.destination(@base_source)
+      @options  = filter_options(current_doc, **options)
+      @name     = current_doc.name
+      @source   = current_doc.path
+
+      logger.debug("base_paths: input → #{@options[:input_base_url]} " \
+                   "output → #{@options[:output_base_path]} " \
+                   "host → #{@options[:input_host]}")
 
       current_options = make_options(@options, options, filter_options(current_doc))
-      current_options[:output_name] ||= Pathname.new(@name).sub_ext(".pdf")
+      current_options[:output_name] ||= Pathname.new(@name).sub_ext('.pdf')
 
       logger.debug("options : #{current_options}")
 
-      return if check_failure(current_options[:disabled], "MakePDF disabled")
+      return if check_failure(current_options[:disabled], 'MakePDF disabled')
 
       return if check_failure(File.extname(@file) != '.html', "#{@file} is not an html")
 
@@ -61,7 +167,7 @@ module MakePDF
       writer = current_options[:writer] || site.options[:writer]
       return if check_failure(writer.nil?, "No writer defined for #{current_doc.name} (#{writer})")
 
-      @writer = MakePDF.const_get(writer.capitalize).new(logger:, **current_options)
+      @writer = MakePDF.const_get(writer.capitalize).new(logger: logger, **current_options)
       @doc = current_doc
     end
 
@@ -70,101 +176,64 @@ module MakePDF
     end
 
     def targets
-      @options[:targets] || ""
+      @options[:targets] || ''
+    end
+
+    def respond_to_missing?(method_name)
+      @options.include?(method_name) ||
+        (!@site.options.nil? && @site.options.include?(method_name)) ||
+        @site.respond_to?(method_name, false)
     end
 
     def method_missing(method_name, *args, **options)
       if @options.include?(method_name)
-        return @options[method_name]
-      elsif not @site.options.nil? and @site.options.include?(method_name)
-        return @site.options[method_name]
+        @options[method_name]
+      elsif !@site.options.nil? && @site.options.include?(method_name)
+        @site.options[method_name]
       elsif @site.respond_to?(method_name, false)
-        return @site.send(method_name, *args, **options)
+        @site.send(method_name, *args, **options)
       else
         super
       end
     end
 
-    def render_option(**options)
-      logger.debug("MakePDF rendering options #{options}")
+    private
 
-      attempted = 0
+    def try(identification, count)
       begin
-        logger.info("processing #{@file}")
-        @writer.process(@file, **options.merge(@options))
+        yield
       rescue => error
-        attempted += 1
-        if attempted <= 2
-          logger.warn("Failed to generate #{@file} retrying #{attempted}")
-          logger.warn("ERROR: #{error}")
+        attempt += 1
+        message = "#{identification} attempt number #{attempt} failed with #{error}"
+        if attempted < count
+          logger.warn("#{message}, retrying")
           retry
         else
-          logger.warn("Skipping generation of #{@file} with #{options}")
-          raise error
+          logger.error("#{message}.")
         end
       end
+    end
+
+    public
+
+    def render_option(**options)
+      logger.debug("MakePDF rendering options #{options}")
+      logger.verbose("processing #{@file}")
+      try("Process #{@file}", 3) do
+        @writer.process(@file, **options.merge(@options))
+      end
+    end
+
+    def filehash
+      return Digest::SHA256.file(@source).hexdigest
     end
 
     def process(**options)
       render_option(**options)
-      unless self.targets.nil?
-        self.targets.split(",").each do |option|
-          render_option(version: option.split(","), **options)
-        end
-      end
-    end
+      return if targets.nil?
 
-    class Site
-      include PathManip
-      attr_reader :options, :site, :logger
-
-      def default_options
-        return { 
-          output_base_path: site.source, 
-          input_location: path_of(site.dest),
-          input_base_url: relative_path_of(site.baseurl[1..]),
-          input_host: site.config["url"].match(Regexp.new("^[^:]*://\([^/]+\)/?.*$"))[1],
-          input_scheme: "file"
-        }.freeze
-      end
-
-      def make_options(options, *more_options)
-        return {} if options.nil?
-
-        [ options, more_options ].flatten
-          .reduce(:merge)
-          .transform_keys do |key|
-            key.to_s.sub("-", "_").to_sym
-          end
-      end
-
-      def initialize(site, **options)
-        config = site.config["make-pdf"]||{}
-        @logger = MakePDF::Logger.new(logger: ::Jekyll.logger, level: (config["log-map-level"] || :debug))
-        @site         = site
-        @options = default_options.merge(make_options(@site.config["make-pdf"], options))
-        @queue        = []
-        logger.debug("Initialized with #{self.options}.")
-      end
-
-      def queue(processor)
-        logger.info("Adding #{processor.name} to queue")
-        @queue.push(processor)
-      end
-
-      def <<(doc)
-        processor = Processor.new(self, doc, **@options)
-        if processor.valid?
-          queue(processor)
-        else
-          logger.info("Skip #{doc.name} => #{processor.reason}")
-        end
-      end
-
-      def process
-        @queue.each do |processor|
-          processor.process(**@options)
-        end
+      targets.split(',').each do |option|
+        render_option(version: option.split(','), **options)
       end
     end
   end
@@ -172,15 +241,15 @@ module MakePDF
   ::Jekyll.logger.info("Loaded #{MakePDF::LOG_NAME} plugin")
 
   ::Jekyll::Hooks.register [:site], :after_init do |site|
-    @@site = MakePDF::Processor::Site.new(site)
-    @@site.logger.info("site :after_init #{@@site}")
+    @pdf_site = MakePDF::MakePDFSite.new(site)
+    @pdf_site.logger.info("site :after_init #{@pdf_site}")
   end
 
   ::Jekyll::Hooks.register [:pages, :documents, :posts], :post_write do |doc|
-    @@site << doc
+    @pdf_site << doc
   end
 
-  ::Jekyll::Hooks.register [:site], :post_write do |site|
-    @@site.process
+  ::Jekyll::Hooks.register [:site], :post_write do
+    @pdf_site.process
   end
 end
--- make_pdf-jekyll.gemspec
+++ make_pdf-jekyll.gemspec
@@ -1,19 +1,18 @@
 Gem::Specification.new do |s|
-  s.name        = "make_pdf-jekyll"
-  s.version     = "0.0.7"
-  s.summary     = "Create PDF along side of HTML files for site."
-  s.description = "Allows that some documents, or pages to have a pdf version pre generated."
-  s.authors     = ["Victor Bogado da Silva Lins"]
-  s.email       = "victor@bogado.net  "
-  s.files       = [
-    "lib/make_pdf/chrome.rb",
-    "lib/make_pdf/firefox.rb",
-    "lib/make_pdf/command_based.rb",
-    "lib/make_pdf/jekyll.rb",
-    "lib/make_pdf.rb"
+  s.name                  = 'make_pdf-jekyll'
+  s.version               = '0.0.9'
+  s.summary               = 'Create PDF along side of HTML files for site.'
+  s.description           = 'Allows that some documents, or pages to have a pdf version pre generated.'
+  s.authors               = ['Victor Bogado da Silva Lins']
+  s.email                 = 'victor@bogado.net  '
+  s.required_ruby_version = '>= 3.0.0'
+  s.files = [
+    'lib/make_pdf/chrome.rb',
+    'lib/make_pdf/firefox.rb',
+    'lib/make_pdf/command_based.rb',
+    'lib/make_pdf/jekyll.rb',
+    'lib/make_pdf.rb'
   ]
-  s.homepage    =
-    "https://github.com/bogado/make_pdf-jekyll"
-  s.license       = "MIT"
+  s.homepage              = 'https://github.com/bogado/make_pdf-jekyll'
+  s.license               = 'MIT'
 end
-
